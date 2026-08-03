3161a34d8ba84956249be12d08d4a7a4ff7553dc
Abd el rahman Mahmoud <abdelrahman.Mahmoud03@eng-st.cu.edu.eg>
2026-08-03 16:34:32 +0300

    feature: server side user foundation

--- Server/src/main/java/EventTicketing/repository/userRepository.java
+++ Server/src/main/java/EventTicketing/repository/userRepository.java
@@ -1,4 +1,20 @@
 package EventTicketing.repository;
 
-public class userRepository {
+import EventTicketing.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface userRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByPhone(String phone);
+
+    Optional<User> findByEmailOrPhone(String email, String phone);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByPhone(String phone);
 }
