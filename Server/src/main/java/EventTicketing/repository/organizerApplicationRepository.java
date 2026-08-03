081c0518cbd03a88d9093cc078d6257a1716542b
Abd el rahman Mahmoud <abdelrahman.Mahmoud03@eng-st.cu.edu.eg>
2026-08-04 00:17:26 +0300

    feature: server side organizer application

--- Server/src/main/java/EventTicketing/repository/organizerApplicationRepository.java
+++ Server/src/main/java/EventTicketing/repository/organizerApplicationRepository.java
@@ -1,4 +1,20 @@
 package EventTicketing.repository;
 
-public class organizerApplicationRepository {
+import EventTicketing.model.OrganizerApplication;
+import EventTicketing.model.enums.OrganizerApplicationStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface organizerApplicationRepository extends JpaRepository<OrganizerApplication, UUID> {
+
+    Optional<OrganizerApplication> findByEmail(String email);
+
+    Optional<OrganizerApplication> findByEmailAndStatus(String email, OrganizerApplicationStatus status);
+
+    List<OrganizerApplication> findByStatus(OrganizerApplicationStatus status);
+
+    boolean existsByEmailAndStatus(String email, OrganizerApplicationStatus status);
 }
