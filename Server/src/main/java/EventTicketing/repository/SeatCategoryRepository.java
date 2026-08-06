31d58e906f447ea9dfefa505fb6cb94e99f40c43
MazenElshazly <mazenelshazly3@gmail.com>
2026-08-06 18:22:07 +0300

    redid some mistakenly deleted files
    ana 3ayz amawet nafse

--- Server/src/main/java/EventTicketing/repository/SeatCategoryRepository.java
+++ Server/src/main/java/EventTicketing/repository/SeatCategoryRepository.java
@@ -1,18 +1,24 @@
 package EventTicketing.repository;
 
+import EventTicketing.model.Event;
 import EventTicketing.model.SeatCategory;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface SeatCategoryRepository extends JpaRepository<SeatCategory, UUID> {
 
+    List<SeatCategory> findByEvent(Event event);
+
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    @Query("SELECT s FROM SeatCategory s WHERE s.id = :id")
+    @Query("select sc from SeatCategory sc where sc.id = :id")
     Optional<SeatCategory> findByIdWithLock(@Param("id") UUID id);
 }
