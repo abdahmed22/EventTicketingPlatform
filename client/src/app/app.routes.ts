08ed14c820ae7ef4eb89736634bef0985e89163d
Abd el rahman Mahmoud <abdelrahman.Mahmoud03@eng-st.cu.edu.eg>
2026-08-08 10:44:13 +0300

    feature: client side login page

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { guestGuard } from './guards/guest.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    canActivate: [guestGuard],
+    loadComponent: () =>
+      import('./components/auth/login/login.component').then((m) => m.LoginComponent)
+  }
+];
